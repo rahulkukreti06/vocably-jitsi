b1d33516fede61d856dd71dc8f9ebaee1a86a83d
Rahul Kukreti <kukretir793@gmail.com>
2025-08-02 12:00:17 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,20 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7821647DB"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-Z7821647DB');
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
